03f431282dc9e50646179e99ca9ab3f03c0c1196
lea <lea.duvigneau@ynov.com>
2020-03-02 00:10:51 +0100

    version3

--- lescript.sh
+++ lescript.sh
@@ -4,14 +4,21 @@ filelock=/tmp/out/*.lock
 
 if test -f $filelock 
 then
-	exit
+	exit 2>>tmp/out/fileerror.log
 else
 	touch /tmp/out/file.lock
 	for files in /tmp/in/*
 	do
-		ls $files
-		gzip -r -f $files
-		mv $files.gz  /tmp/out/
+		if test -e -f $files
+		then
+			ls $files
+			gzip -r -f $files
+			mv $files.gz  /tmp/out/
+		else
+			rm /tmp/out/file.lock
+			echo $? 2>>tmp/out/fileerror.log
+		fi
 	done
 	rm /tmp/out/file.lock
+	echo "le fichier $files a été déplacé dans /tmp/out" 2>> /tmp/out/fileerror.log
 fi
